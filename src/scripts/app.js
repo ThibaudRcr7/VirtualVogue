75e5808d0f90ebcd449cd1dca47c87247d4906f3
Thibaud Rocour <thibaudrcr@gmail.com>
2023-05-27 19:01:51 +0200

    Burger menu + Première section

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -1,1 +1,14 @@
 console.info('Hello world');
+
+const openburger = document.querySelector(".header__nav--burger");
+const closeburger = document.querySelector(".header__nav--closeburger");
+
+openburger.addEventListener("click", ()=>{
+
+    document.querySelector(".header__nav").classList.add("open");
+})
+
+closeburger.addEventListener("click", ()=>{
+
+    document.querySelector(".header__nav").classList.remove("open");
+})
