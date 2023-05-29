c6c5a47c561b9da56bc081043a08526feee19034
Thibaud Rocour <thibaudrcr@gmail.com>
2023-05-29 19:50:55 +0200

    Fin de la Section 4

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -1,21 +1,7 @@
 "use strict";
 
-
-
-import { gsap } from "gsap";
-import ScrollTrigger from "gsap/dist/ScrollTrigger";
-
- gsap.registerPlugin(ScrollTrigger);
- gsap.to(".stats__block", {
-   x: 250,
-   scrollTrigger: {
-     trigger: ".stats__block",
-     start: "top bottom",
-     markers: "true",
-   },
-   toggleActions: "play restart restart restart"
- });
-
+// import { gsap } from "gsap";
+// import ScrollTrigger from "gsap/dist/ScrollTrigger";
 
 const openburger = document.querySelector(".header__nav--burger");
 const closeburger = document.querySelector(".header__nav--closeburger");
@@ -28,5 +14,15 @@ openburger.addEventListener("click", ()=>{
 closeburger.addEventListener("click", ()=>{
 
     document.querySelector(".header__nav").classList.remove("open");
-})
+});
 
+// gsap.registerPlugin(ScrollTrigger);
+// gsap.to(".stats__block", {
+//   x: 250,
+//   scrollTrigger: {
+//     trigger: ".stats__block",
+//     start: "top bottom",
+//     markers: "true",
+//   },
+//   toggleActions: "play restart restart restart"
+// });
