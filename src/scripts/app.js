c142e9b77ff708501f562b14db037f0dc5ff5885
Thibaud Rocour <thibaudrcr@gmail.com>
2023-06-06 14:36:27 +0200

    Toutes pages finies manque ajustements

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -1,6 +1,8 @@
 "use strict";
 
-import { gsap } from "gsap";
+import {
+	gsap
+} from "gsap";
 import ScrollTrigger from "gsap/dist/ScrollTrigger";
 
 
@@ -9,25 +11,55 @@ gsap.registerPlugin(ScrollTrigger);
 const circle = document.querySelector("svg circle");
 
 gsap.to(circle, {
-  strokeDashoffset: 189,
-  duration: 2,
-  scrollTrigger: {
-    trigger: "#statsBlock",
-    start: "top 80%",
-    end: "bottom 80%",
-    scrub: true
-  }
+	strokeDashoffset: 189,
+	duration: 2,
+	scrollTrigger: {
+		trigger: "#statsBlock",
+		start: "top 80%",
+		end: "bottom 80%",
+		scrub: true
+	}
 });
+
+var customCursor = document.getElementById('custom-cursor');
+
+document.addEventListener('mousemove', function(e) {
+	// Ajouter un délai de 100 millisecondes (ajustable selon vos besoins)
+	setTimeout(function() {
+		var x = e.clientX;
+		var y = e.clientY;
+		customCursor.style.left = x + 'px';
+		customCursor.style.top = y + 'px';
+	}, 100);
+});
+
 
 const openburger = document.querySelector(".header__nav--burger");
 const closeburger = document.querySelector(".header__nav--closeburger");
 
-openburger.addEventListener("click", ()=>{
+openburger.addEventListener("click", () => {
 
-    document.querySelector(".header__nav").classList.add("open");
+	document.querySelector(".header__nav").classList.add("open");
 })
 
-closeburger.addEventListener("click", ()=>{
+closeburger.addEventListener("click", () => {
 
-    document.querySelector(".header__nav").classList.remove("open");
+	document.querySelector(".header__nav").classList.remove("open");
+});
+
+const arrowContainer = document.getElementById('arrow-container');
+
+arrowContainer.addEventListener('click', () => {
+	window.scrollTo({
+		top: 0,
+		behavior: 'smooth'
+	});
+});
+
+window.addEventListener('scroll', () => {
+	if (window.scrollY > 0) {
+		arrowContainer.classList.add('clicked');
+	} else {
+		arrowContainer.classList.remove('clicked');
+	}
 });
